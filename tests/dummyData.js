d3db424da9c0023f743322412d222703d67cfdae
avichauhan6832 <avi.r.chauhan6832@gmail.com>
2020-02-29 18:51:47 +0530

    basic test functions

---
+++ tests/dummyData.js
@@ -0,0 +1,16 @@
+var data = {
+	"firstName": "test3",
+	"middleName": "test3",
+	"lastName": "test3",
+	"email": "test3@test.com",
+	"mobileNumber": "1234567892",
+	"addess": "test test test",
+	"city": "ahmedabad",
+	"state": "guj",
+	"postalCode": "123456",
+	"education": "Graduate",
+	"gender": "Male",
+	"occupation": "Salaried"
+}
+
+module.exports = data;
